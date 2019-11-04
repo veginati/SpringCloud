ac0d2effdd57a06b3b7c28834df204ddb9d5694c
Vamsi Veginati <vamsiveginati@gmail.com>
2019-11-03 23:49:47 -0500

    naming service to add dynamic increase and decrease in nodes

--- currency-conversion-service/src/main/java/com/microapp/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microapp/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.microapp.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.microapp")
 @EnableFeignClients("com.microapp")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-exchange-service/src/main/java/com/microapp/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microapp/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,14 +3,15 @@ package com.microapp.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.microapp")
 @EntityScan("com.microapp")
-//@EnableAutoConfiguration
 @EnableJpaRepositories("com.microapp")
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
