91d19f318dd270751974e898950c7367ee6ffee2
vamsiveginati <veginativamsi@gmail.com>
2022-10-08 13:42:50 -0700

    push files

--- spring-cloud-config-server-2/src/main/java/com/vveginati/microservice/springcloudconfigserver2/SpringCloudConfigServer2Application.java
+++ spring-cloud-config-server-2/src/main/java/com/vveginati/microservice/springcloudconfigserver2/SpringCloudConfigServer2Application.java
@@ -2,7 +2,9 @@ package com.vveginati.microservice.springcloudconfigserver2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServer2Application {
 
